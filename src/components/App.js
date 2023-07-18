2246ffe9e767681ce1bd300f958736ece3f3d3c4
gilbert-ku <gilbert45ku@gmail.com>
2023-07-18 18:57:25 +0300

    completed assignment

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,8 @@
 import React from "react";
-
+import NavBar from "./NavBar";
+import Home from "./Home";
+import About from "./About";
+// import {username, city, image} from "./scr/data/user"
 function App() {
   return (
     <div>
